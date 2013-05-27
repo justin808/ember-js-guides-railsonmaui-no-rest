acdbce65d1960423cde4b5439ab44ada3c42d9e6
Justin Gordon <justin.gordon@gmail.com>
2013-05-26 14:04:21 -1000

    Finished set of files.

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-Starter set of files for the Tom Dale Tutorial
+Finished set of files for the Tom Dale Tutorial
 at [Building an App with Ember.js](http://www.youtube.com/watch?feature=player_embedded&v=Ga99hMi7wfY)
 including a couple fixture records.
 
 See article and live example at [Ember.js Hello World](http://www.railsonmaui.com/http://octopress.dev/blog/2013/05/26/learning-ember-dot-js/).
 
-
+Only differeces are:
+1. Slight bit of styling added in style.css
+2. Moved fixtures to separate file.
 
 
 
--- js/app.js
+++ js/app.js
@@ -5,7 +5,48 @@ App.Store = DS.Store.extend({
   adapter: 'DS.FixtureAdapter'
 });
 
+App.Router.map(function() {
+  this.resource('about');
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
+});
 
+App.Post = DS.Model.extend({
+  title: DS.attr('string'),
+  author: DS.attr('string'),
+  intro: DS.attr('string'),
+  extended: DS.attr('string'),
+  publishedAt: DS.attr('date')
+});
 
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return App.Post.find();
+  }
+});
 
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+  edit: function() {
+    this.set('isEditing', true);
+  },
+  doneEditing: function() {
+    this.set('isEditing', false);
+  }
+});
 
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+   this.transitionTo('posts');
+  }
+});
+
+Ember.Handlebars.registerBoundHelper('date', function(date) {
+   return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+Ember.Handlebars.registerBoundHelper('markdown', function(input) {
+  return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
+});
